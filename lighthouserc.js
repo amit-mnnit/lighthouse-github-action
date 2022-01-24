de7ad0f6886dc383da6318950fcb9a6d3fa6e0f4
Amit Kumar <amitkumar@Amits-MacBook-Pro.local>
2022-01-24 16:21:16 +0530

    lighthouse integrate

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,6 +7,9 @@ module.exports = {
     assert: {
       assertions: {
         "first-contentful-paint": ["warn", { maxNumericValue: 4000 }],
+        "categories:performance": ["warn", { "minScore": 0.9 }],
+        "categories:accessibility": ["error", { "minScore": 0.9 }],
+        "categories:seo": ["error", { "minScore": 1 }]
       },
     },
     upload: {
