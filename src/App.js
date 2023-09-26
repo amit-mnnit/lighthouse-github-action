bc6a842b190b895a09f5b91575c21f76268411d8
Amit Kumar <33446537+amit-mnnit@users.noreply.github.com>
2023-09-26 16:57:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome amit
+          Welcome amit 1
         </p>
       </header>
     </div>
